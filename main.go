17ec0dc338c84e797e6a05ee629aa53a0114bd01
olzh2102 <olzhas2102@yahoo.com>
2023-09-25 09:29:55 +0200

    database connection

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/olzh2102/golang-hotel-reservation/api"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://localhost:27017"
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("mongo client:", client)
+
 	listenAddr := flag.String("listenAddr", ":3000", "The listen address of the API server")
 	flag.Parse()
 
