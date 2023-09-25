616393ae5c1909092e98c2c5ac1ed13b578a3478
olzh2102 <olzhas2102@yahoo.com>
2023-09-25 10:07:31 +0200

    insert an user to db

--- main.go
+++ main.go
@@ -8,18 +8,32 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/olzh2102/golang-hotel-reservation/api"
+	"github.com/olzh2102/golang-hotel-reservation/types"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
 
 func main() {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("mongo client:", client)
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+
+	user := types.User{
+		FirstName: "James",
+		LastName:  "Longstaff",
+	}
+	res, err := coll.InsertOne(ctx, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("inserted user: ", res)
 
 	listenAddr := flag.String("listenAddr", ":3000", "The listen address of the API server")
 	flag.Parse()
